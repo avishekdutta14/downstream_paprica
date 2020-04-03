2ba1fdbe8897494aef16f77dfd350ca1d9af9de4
avishekdutta14 <35264497+avishekdutta14@users.noreply.github.com>
2020-04-02 20:25:56 -0700

    Add files via upload

---
+++ taxa_percentage_for_normalized_edge.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#defining percentage function
+
+def percentage(x):
+     return x/x.sum() * 100
+	
+taxa = pd.read_csv('normalized_edge.csv', index_col=0)
+
+#applying percentage function
+
+taxa.iloc[:, :-7] = percentage(taxa.iloc[:, :-7]) # selecting all the columns except the last seven where taxonomy is there
+
+taxa.to_csv('taxa_abundance_normalized_percent.csv')
+
+print("The output is in taxa_abundance_normalized_percent.csv")
+
