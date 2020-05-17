3a7c4c72f63f2cfbe16625c3c85784ed3239a97a
avishekdutta14 <35264497+avishekdutta14@users.noreply.github.com>
2020-05-17 13:14:18 -0700

    Corrected for python version

--- taxzyme.py
+++ taxzyme.py
@@ -1,55 +1,69 @@
 import pandas as pd
 import os
 
-#merging all the transposed file
 path = '.'
 files_in_dir = [f for f in os.listdir(path) if f.endswith('.ec.csv')]
+for filename in files_in_dir:
+    # We save an augmented filename later, 
+    # so using splitext is useful for more
+    # than just checking the extension.
+    prefix, ext = os.path.splitext(filename)
+    if ext.lower() != '.csv':
+        continue
+    # Load the data into a dataframe
+    df = pd.read_csv(filename, 
+                               header=None, 
+                               index_col=None, 
+                               parse_dates=False)
+    # Transpose is easy, but you could do TONS
+    # of data processing here. pandas is awesome.
+    df_transposed = df.T
+    # Save to a new file with an augmented name 
+    df_transposed.to_csv(prefix+'_T'+ext, header=False, index=False)
+
+
+#merging all the transposed file
+path = '.'
+files_in_dir = [f for f in os.listdir(path) if f.endswith('.ec_T.csv')]
 for filenames in files_in_dir:
     edge_data = pd.read_csv(filenames)
     edge_data.to_csv('merged_ec_data.csv', mode='a')
 
+
+#removal of all ec_T.csv file 
+
+dir_name = "."
+test = os.listdir(dir_name)
+
+for item in test:
+    if item.endswith("ec_T.csv"):
+        os.remove(os.path.join(dir_name, item))
+
+
 #reading the file and renaming the column for finding the index later
 df = pd.read_csv('merged_ec_data.csv')
-df.rename(columns={'Unnamed: 0.1': 'OTU'}, inplace=True)
 
-#retaining the columns having enzymes related to sulfur cycle
+df1 = df.drop(['Unnamed: 0'], axis=1)
 
-df1 = df[['OTU','1.8.1.2', '1.8.7.1', '1.8.99.1', '1.97.1.3', '1.13.11.5', '2.8.1.5', '1.8.5.2' , '2.8.1.1', '1.8.99.3', '1.8.2.1', '1.8.3.1', '3.5.5.8', '4.4.1.1', '4.4.1.8', '1.8.1.6', '4.4.1.15', '4.2.1.1', '4.4.1.2', '1.13.11.20', '3.1.6.1']]
+df1.rename(columns={'Unnamed: 0.1': 'OTU'}, inplace=True)
 
-df2 = df1[df1.OTU != 'Unnamed: 0'] # removing the columns starting with Unnamed: 0
-
-df2 =df2.astype(float).round(0) # converting the values into floats
-
-df2 = df2.groupby(['OTU']).sum() # adding the values from similar taxa
-
-df2['total'] = df2.iloc[:,0] + df2.iloc[:,1] + df2.iloc[:,2] + df2.iloc[:,3] + df2.iloc[:,4] + df2.iloc[:,5] + df2.iloc[:,6] + df2.iloc[:,7] + df2.iloc[:,8] + df2.iloc[:,9] + df2.iloc[:,10] + df2.iloc[:,11] + df2.iloc[:,12] + df2.iloc[:,13] + df2.iloc[:,14] + df2.iloc[:,15] + df2.iloc[:,16] + df2.iloc[:,17] + df2.iloc[:,18] + df2.iloc[:,19] # making total
-
-df2.drop(df2[df2.total == 0].index, inplace = True) # removing 0 from total to get exact number of taxa contributing particular cycle
-
-df2.to_csv('sulfur.csv')
-
-df3 = pd.read_csv('taxa_abundance.csv')
-
-result = df2.merge(df3,on='OTU',how='left')
-result.to_csv('sulfur_taxa.csv')
-
-#print (result)
+df2 = df1[df1.OTU != 'Unnamed: 0']
 
 
-#retaining the columns having enzymes related to sulfur cycle
+#df2['OTU'] = df2['OTU'].astype(float)
 
-nitrate = df[['OTU','1.7.99.4','1.7.1.4','1.7.7.1','1.7.2.1','1.7.1.1','1.7.99.1','1.7.2.2']]
-nitrate = nitrate[nitrate.OTU != 'Unnamed: 0']
-nitrate = nitrate.astype(float).round(0)
-nitrate = nitrate.groupby(['OTU']).sum()
-nitrate['total'] = nitrate.iloc[:,0] + nitrate.iloc[:,1] + nitrate.iloc[:,2] + nitrate.iloc[:,3] + nitrate.iloc[:,4] + nitrate.iloc[:,5] + nitrate.iloc[:,6]
+df3 = df2.astype(float)
 
-nitrate.drop(nitrate[nitrate.total == 0].index, inplace = True)
+#print(df3)
 
-#print (nitrate)
-nitrate.to_csv('nitrate.csv')
-result1 = nitrate.merge(df3,on='OTU',how='left')
-print (result1)
-result1.to_csv('nitrate_taxa.csv')
+df4= df3.groupby(['OTU']).sum()
 
-os.remove ('merged_ec_data.csv') # removal of this file is required otherwise adding up starts
+df5 = pd.read_csv('taxa_abundance.csv')
+
+result = df5.merge(df4,on='OTU',how='left')
+
+result.to_csv('taxzyme_view.csv', index = False)
+
+os.remove ('merged_ec_data.csv')
+
+print("The output is in taxzyme_view.csv file")
